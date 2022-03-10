278cf9be4a19e1aff27dca525ff28d120e9b69e4
Boris Sáez Milanez <55374102+sharshakap@users.noreply.github.com>
2022-03-10 07:44:43 -0300

    feat: back_urls

--- routes/views.route.js
+++ routes/views.route.js
@@ -8,5 +8,17 @@ router.get('/', function (req, res) {
 router.get('/detail', function (req, res) {
 	res.render('detail', req.query);
 });
+//failure
+router.get('/failure', function (req, res) {
+	res.render('failure');
+});
+//success
+router.get('/success', function (req, res) {
+	res.render('success');
+});
+//pending
+router.get('/pending', function (req, res) {
+	res.render('pending');
+});
 
 module.exports = router;
