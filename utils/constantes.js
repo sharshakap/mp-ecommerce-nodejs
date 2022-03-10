aa2bc8ea7c5a08be62e6c27809bb67c3eeb84d63
Boris Sáez Milanez <55374102+sharshakap@users.noreply.github.com>
2022-03-10 18:42:17 -0300

    configurando para heroku

--- utils/constantes.js
+++ utils/constantes.js
@@ -1,5 +1,7 @@
-const APIURL = 'http:///localhost:8080/api/';
-const FRONTURL = 'http:///localhost:8080/';
+// const APIURL = 'http:///localhost:8080/api/';
+// const FRONTURL = 'http:///localhost:8080/';
+const APIURL = 'https://mp-boris-examen.herokuapp.com/api/';
+const FRONTURL = 'https://mp-boris-examen.herokuapp.com//';
 
 module.exports = {
 	APIURL,
